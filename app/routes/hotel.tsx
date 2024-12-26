c04b92dd5226702849c66a534d4442805c2afbb6
Ankur Kaushal <97340597+Avenster@users.noreply.github.com>
2024-12-26 23:26:48 +0530

    Hotel Images added

---
+++ app/routes/hotel.tsx
@@ -0,0 +1,12 @@
+import Home from "~/pages/HomePage"
+import Navbar from "~/components/Header"
+
+export default function HotelHomePage() {
+  return (
+    <div>
+        <Navbar/>
+        <Home/>
+      
+    </div>
+  )
+}
